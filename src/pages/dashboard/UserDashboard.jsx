4c913ffb46c3f13810129de041dae9667cb5e726
Ikramul Hasan Rakib <hasanrakib3590@gmail.com>
2024-11-09 22:09:59 +0600

    ...

--- src/pages/dashboard/UserDashboard.jsx
+++ src/pages/dashboard/UserDashboard.jsx
@@ -31,36 +31,11 @@ const UserDashboard = () => {
   return (
     <div className="space-y-5 md:h-screen mx-5 my-2 flex flex-col justify-between bg-white">
       <div>
-        {/* General navigation links */}
         <div className="nav__logo">
           <Link to="/">Galore</Link>
           <p className="text-sm">User Dashboard</p>
         </div>
-        <ul className="nav__links space-y-5 pt-5">
-          <li className="link">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="link">
-            <Link to="/shop">Shop</Link>
-          </li>
-          <li className="link">
-            <a
-              href="https://github.com/ih-rakib/Profile/blob/master/Projects/Readme.md"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Pages
-            </a>
-          </li>
-          <li className="link">
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-
-        {/* Divider between general and dashboard links */}
         <hr className="mt-5" />
-
-        {/* Dashboard-specific links */}
         <ul className="space-y-5 pt-5">
           {navItems.map((item) => (
             <li key={item.path}>
